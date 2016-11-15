dcf417d4c4cb15320c703927d169b74176b61d20
ZacJocelyn <zacjocelyn@gmail.com>
2016-11-15 14:03:23 -0700

    got pic to show

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('button').click(function(event) {
     event.preventDefault();
-    $.get('http://galvanize-cors-proxy.herokuapp.com/https://api.brewerydb.com/v2/beer/random?key=c2c91700d2822349080b91377047e76d&format=json', displayBeer);
+    $.get('https://galvanize-cors-proxy.herokuapp.com/https://api.brewerydb.com/v2/beer/random?key=c2c91700d2822349080b91377047e76d&format=json', displayBeer);
   });
 });
 function displayBeer(result) {
@@ -19,14 +19,23 @@ function description(result) {
   var $p = $('<p>' + desc + '</p>');
   return $p;
 }
+function displayImg() {
+  var $img = $('<img class="result-pic" src ="http://beerhold.it/' + randomNum() + '/' + randomNum() + '"/>');
+  return $img;
+}
 
 function append(result) {
   var $section = $('.beer-display');
   $section.html('');
-  $section.append(displayName(result), description(result));
+  $section.append(displayName(result), displayImg(), description(result));
   $('h5').html('');
   $('h5').text('Thanks for getting a beer!!');
 }
 function remove() {
   $('main').html('');
+}
+function randomNum() {
+  var num = Math.random();
+  num = Math.floor(num * 1000);
+  return num.toString();
 }
