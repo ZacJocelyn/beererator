cac6d4de0f28dafefeb44b8f8ae8b09690c58611
ZacJocelyn <zacjocelyn@gmail.com>
2016-11-16 09:31:46 -0700

    added google maps api

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     $('button').click(function(event) {
         event.preventDefault();
         $.get('https://galvanize-cors-proxy.herokuapp.com/https://api.brewerydb.com/v2/beer/random?key=c2c91700d2822349080b91377047e76d&format=json', displayBeer);
+        $.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB6Krk9RvBv_qAPxlab5VzP0Ybae5Skjqw', updateMap)
     });
         $.get('https://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/js?key=AIzaSyDc7bvpogNBwNN6YHrCnPgiJKoUVJC7R1g&libraries=places')
 });
