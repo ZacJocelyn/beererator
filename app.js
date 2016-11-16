f2164f51a91049f2beffea4d00f79cb4781c42e3
ZacJocelyn <zacjocelyn@gmail.com>
2016-11-16 10:37:21 -0700

    got the map to auto center based on address

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ $(document).ready(function() {
   };
     $('button').click(function(event) {
         event.preventDefault();
+        var $input = $('input');
+        var address = $input.val();
+        $input.val('');
+        $('#map').show();
         $.get('https://galvanize-cors-proxy.herokuapp.com/https://api.brewerydb.com/v2/beer/random?key=c2c91700d2822349080b91377047e76d&format=json', displayBeer);
-        $.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB6Krk9RvBv_qAPxlab5VzP0Ybae5Skjqw', updateMap)
+        $.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ address +'key=AIzaSyB6Krk9RvBv_qAPxlab5VzP0Ybae5Skjqw', initMap)
     });
         $.get('https://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/js?key=AIzaSyDc7bvpogNBwNN6YHrCnPgiJKoUVJC7R1g&libraries=places')
+        autocomplete = new google.maps.places.Autocomplete(input, options);
 });
 
 // to display the beer info
@@ -51,3 +56,15 @@ function randomNum() {
     return num.toString();
 }
 // to display the location of a liqor store near you
+var geocoder;
+var map;
+function initMap(obj) {
+  var lat = obj.results[0].geometry.location.lat;
+  var lng = obj.results[0].geometry.location.lng;
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: lat, lng: lng},
+    zoom: 15
+  });
+  return lat, lng;
+}
