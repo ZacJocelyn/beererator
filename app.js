cbad1d7176a9d78d0ef4c30da6f179b9b89f41aa
ZacJocelyn <zacjocelyn@gmail.com>
2016-11-17 15:33:02 -0700

    got everyhting center aligned

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         $.get('https://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/js?key=AIzaSyDc7bvpogNBwNN6YHrCnPgiJKoUVJC7R1g&libraries=places', initialize);
     });
     $('.brand-logo').click(function(event) {
-      window.location.reload();
+        window.location.reload();
     })
 
     autocomplete = new google.maps.places.Autocomplete(input, options);
@@ -47,7 +47,7 @@ function displayBeer(result) {
 function append(result) {
     var $section = $('.beer-display');
 
-var beerCard = `<div class="row">
+    var beerCard = `<div class="row">
   <div class="beer-card col s6 offset-s3">
     <div class="card">
       <div class="card-image">
@@ -63,7 +63,7 @@ var beerCard = `<div class="row">
   </div>
 </div>`;
 
-    $section.append(beerCard)//displayName(result), displayImg(), description(result));
+    $section.append(beerCard);
     $('h5').html('');
     $('h5').text('Thanks for getting a beer!!');
 }
@@ -73,8 +73,8 @@ function remove() {
 }
 
 function randomNum() {
-    var numArr =[
-      200, 300, 400, 500, 600, 700, 200, 600, 500, 300
+    var numArr = [
+        200, 300, 400, 500, 600, 700, 200, 600, 500, 300
     ];
     var num = Math.random();
     num = Math.floor(num * 10);
@@ -86,6 +86,7 @@ var geocoder;
 var map;
 var lng;
 var lat;
+
 function initMap(obj) {
     lat = obj.results[0].geometry.location.lat;
     lng = obj.results[0].geometry.location.lng;
@@ -102,10 +103,10 @@ function initMap(obj) {
 
 }
 
-///////////////////////////////////////////////
 var allPlaceName = [];
 var placeType = [];
 var openClosed = [];
+
 function initialize() {
     var pyrmont = new google.maps.LatLng(lat, lng);
     map = new google.maps.Map(document.getElementById('map'), {
@@ -152,13 +153,26 @@ function createMarker(place) {
 // display a list of places and if they are open
 
 function displayList() {
-  console.log(allPlaceName, openClosed)
-  for (var i = 0; i < allPlaceName.length; i++) {
-      if (openClosed[i] === true) {
-        $('#locations').append($('<li id="open">' + allPlaceName[i] + '<p class="open">open!</p></li>'));
-      }
-      else if (openClosed[i] === false) {
-        $('#locations').append($('<li id="closed">' + allPlaceName[i] + '<p class="closed">closed :(</p></li>'));
+    console.log(allPlaceName, openClosed)
+    for (var i = 0; i < allPlaceName.length; i++) {
+        if (openClosed[i] === true) {
+            $('#locations').append($(`<div id="open" class="z-depth-2">
+        <li>
+        ${allPlaceName[i]}
+        </li>
+        <div class="chip open green white-text z-depth-2">
+        OPEN
+        </div>
+        </div>`));
+        } else if (openClosed[i] === false) {
+            $('#locations').append($(`<div id="closed" class="z-depth-2">
+        <li>
+        ${allPlaceName[i]}
+        </li>
+        <div class="chip closed red white-text z-depth-2">
+        CLOSED
+        </div>
+        </div>`));
+        }
     }
-  }
 }
